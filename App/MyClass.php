cbc904c10d32dcf9f2297e4b25a5234727c9163c
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-12-04 13:33:52 +0300

    end

---
+++ App/MyClass.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App;
+
+
+class MyClass
+{
+    public $age = 25;
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use App\MyClass;
+
+$class = new MyClass();
+echo $class->age;
